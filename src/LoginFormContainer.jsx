de4f234d39b995566aca1acb3cae11bdb2252c31
daadaadaah <daadaadaah@gmail.com>
2020-07-14 00:35:21 +0900

    [preGreen][subRefactor] Refactor a LoginFormContainer  for adding LoginForm

--- src/LoginFormContainer.jsx
+++ src/LoginFormContainer.jsx
@@ -2,32 +2,19 @@ import React from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import LoginForm from './LoginForm';
+
 import { requestLogin } from './actions';
 
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
-  function handleClick() {
+  function handleSubmit() {
     dispatch(requestLogin()); // TODO : 상태가 필요하네? -> Redux로 가자!
   }
 
   return (
-    <>
-      <div>
-        <label htmlFor="login-email">E-mail</label>
-        <input type="email" id="login-email" />
-      </div>
-      <div>
-        <label htmlFor="login-password">Password</label>
-        <input type="password" id="login-password" />
-      </div>
-      <button
-        type="button"
-        onClick={handleClick}
-      >
-        Log In
-      </button>
-    </>
+    <LoginForm onSubmit={handleSubmit} />
 
   );
 }
