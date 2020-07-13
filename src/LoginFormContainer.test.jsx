caef5385c1f0b0e7163e62a38b963ffb2a08c90d
daadaadaah <daadaadaah@gmail.com>
2020-07-13 22:24:12 +0900

    [Red] Add a LoginFormContainer test

---
+++ src/LoginFormContainer.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import LoginFormContainer from './LoginFormContainer';
+
+describe('LoginFormContainer', () => {
+  it('renders input controls', () => {
+    const { getByLabelText } = render(
+      <LoginFormContainer />,
+    );
+
+    expect(getByLabelText('Username')).not.toBeNull();
+    expect(getByLabelText('Password')).not.toBeNull();
+  });
+});
