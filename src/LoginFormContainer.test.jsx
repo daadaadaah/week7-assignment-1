15a864653ef1c9869cea6791f5fab0f092a9b73a
daadaadaah <daadaadaah@gmail.com>
2020-07-14 04:57:26 +0900

    [Red+Green] Add a LoginFormContainer test for listens change event and Implement it

--- src/LoginFormContainer.test.jsx
+++ src/LoginFormContainer.test.jsx
@@ -31,6 +31,21 @@ describe('LoginFormContainer', () => {
     expect(getByLabelText('Password').value).toBe('1234');
   });
 
+  it('listens change events', () => {
+    const { getByLabelText } = render(
+      <LoginFormContainer />,
+    );
+
+    fireEvent.change(getByLabelText('E-mail'), {
+      target: { value: 'new email' },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeLoginField',
+      payload: { name: 'email', value: 'new email' },
+    });
+  });
+
   it('renders [Log In] button', () => {
     const { getByText } = render(
       <LoginFormContainer />,
