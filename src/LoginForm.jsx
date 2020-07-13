0748d834accacf49fe914522d97bf93b43d48c50
daadaadaah <daadaadaah@gmail.com>
2020-07-14 03:11:51 +0900

    Update LoginForm for adding fields

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-export default function LoginForm({ onChange, onSubmit }) {
+export default function LoginForm({ fields, onChange, onSubmit }) {
+  const { email, password } = fields;
+
   function handleChange(event) {
     const { target: { name, value } } = event;
     onChange({ name, value });
@@ -10,11 +12,11 @@ export default function LoginForm({ onChange, onSubmit }) {
     <>
       <div>
         <label htmlFor="login-email">E-mail</label>
-        <input type="email" id="login-email" name="email" onChange={handleChange} />
+        <input type="email" id="login-email" name="email" value={email} onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="login-password">Password</label>
-        <input type="password" id="login-password" name="password" onChange={handleChange} />
+        <input type="password" id="login-password" name="password" value={password} onChange={handleChange} />
       </div>
       <button type="button" onClick={onSubmit}>Log In</button>
     </>
